ee2944c4f2b453aeb6fb3eeacefcaeb632de6503
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-20 16:36:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/widgetize-pages-light/tags/1.0@587855 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/otw_sidebar_info.php
@@ -0,0 +1,20 @@
+<?php
+/** Plugin Information
+  *
+  */
+?>
+<div class="wrap">
+	<div id="icon-edit" class="icon32"><br/></div>
+	<h2>
+		<?php _e('Info') ?>
+	</h2>
+	<div class="form-wrap" id="poststuff">
+		<div id="post-body">
+				<div id="post-body-content">
+					<div class="form-field">
+						<p><?php _e( 'Check out the <a href="http://otwthemes.com/online-documentation-widgetize-pages-light/?utm_source=wp.org&utm_medium=admin&utm_content=docs&utm_campaign=wpl">Online documentation</a> for this plugin<br /><br /> Upgrade to the full version of <a href="http://otwthemes.com/product/sidebar-widget-manager-for-wordpress/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Sidebar and Widget Manager</a> | <a href="http://otwthemes.com/demos/sidebar-widget-manager/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Demo site</a> | <a href="http://www.youtube.com/watch?v=WT9UK1eX4C8">Video overview</a>' );?></p>
+					</div>
+				</div>
+		</div>
+	</div>
+</div>
