ce95942fb1d29b35ecb520c30e6a7ab7ac1ac8ee
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-24 05:34:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/widgetize-pages-light/trunk@589645 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/otw_list_sidebars.php
+++ include/otw_list_sidebars.php
@@ -36,7 +36,8 @@ if( is_array( $otw_sidebar_list ) && count( $otw_sidebar_list ) ){
 }
 
 ?>
-<div class="updated"><p>Check out the <a href="http://otwthemes.com/online-documentation-widgetize-pages-light/?utm_source=wp.org&utm_medium=admin&utm_content=docs&utm_campaign=wpl">Online documentation</a> for this plugin<br /><br /> Upgrade to the full version of <a href="http://otwthemes.com/product/sidebar-widget-manager-for-wordpress/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Sidebar and Widget Manager</a> | <a href="http://otwthemes.com/demos/sidebar-widget-manager/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Demo site</a> | <a href="http://www.youtube.com/watch?v=WT9UK1eX4C8">Video overview</a></p></div>
+<div class="updated"><p>Check out the <a href="http://otwthemes.com/online-documentation-widgetize-pages-light/?utm_source=wp.org&utm_medium=admin&utm_content=docs&utm_campaign=wpl">Online documentation</a> for this plugin<br /><br /> Upgrade to the full version of <a href="http://otwthemes.com/product/sidebar-widget-manager-for-wordpress/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Sidebar and Widget Manager</a> | <a href="http://otwthemes.com/demos/sidebar-widget-manager/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Demo site</a> | <a href="http://www.youtube.com/watch?v=WT9UK1eX4C8">Video overview</a><br /><br />
+Follow on <a href="http://twitter.com/OTWthemes">Twitter</a> | <a href="http://www.facebook.com/pages/OTWthemes/250294028325665">Facebook</a> | <a href="http://www.youtube.com/OTWthemes">YouTube</a> | <a href="https://plus.google.com/117222060323479158835/about">Google+</a></p></div>
 <?php if ( $message ) : ?>
 <div id="message" class="updated"><p><?php echo $message; ?></p></div>
 <?php endif; ?>
