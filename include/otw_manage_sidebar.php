e81e80c4cccd877099e3f745fbeeb583bdb72de8
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-02 15:49:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/widgetize-pages-light/trunk@607019 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/otw_manage_sidebar.php
+++ include/otw_manage_sidebar.php
@@ -147,6 +147,7 @@ function otw_sidebar_item_attributes( $tag, $item_type, $item_id, $sidebar_data,
 ?>
 <div class="updated"><p>Check out the <a href="http://otwthemes.com/online-documentation-widgetize-pages-light/?utm_source=wp.org&utm_medium=admin&utm_content=docs&utm_campaign=wpl">Online documentation</a> for this plugin<br /><br /> 
 Upgrade to the full version of <a href="http://otwthemes.com/product/sidebar-widget-manager-for-wordpress/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Sidebar and Widget Manager</a> | <a href="http://otwthemes.com/demos/sidebar-widget-manager/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">Demo site</a> | <a href="http://www.youtube.com/watch?v=WT9UK1eX4C8">Video overview</a><br /><br />
+Sidebar and Widget Manager has easy column <a href="http://otwthemes.com/widgetizing-pages-in-wordpress-has-never-been-easier/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wpl">interface for Widgetizing Pages</a><br /><br />
 Follow on <a href="http://twitter.com/OTWthemes">Twitter</a> | <a href="http://www.facebook.com/pages/OTWthemes/250294028325665">Facebook</a> | <a href="http://www.youtube.com/OTWthemes">YouTube</a> | <a href="https://plus.google.com/117222060323479158835/about">Google+</a></p></div>
 <div class="wrap">
 	<div id="icon-edit" class="icon32"><br/></div>
