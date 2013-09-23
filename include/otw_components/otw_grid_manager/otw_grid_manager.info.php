028d3fb864d3529b16f2d02676d2fea3e0aebebb
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-23 08:02:03 +0000

    v 1.10
    git-svn-id: https://plugins.svn.wordpress.org/widgetize-pages-light/tags/1.10@777124 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/otw_components/otw_grid_manager/otw_grid_manager.info.php
@@ -0,0 +1,16 @@
+<?php
+/**
+Component Name: OTW Grid Manager
+Plugin URI: http://OTWthemes.com
+Description:  OTW Grid Manager
+Author: OTWthemes.com
+Version: 0.001 light version
+Author URI: http://themeforest.net/user/OTWthemes
+*/
+
+$otw_component = array();
+$otw_component['name']       = 'OTW Grid Manager';
+$otw_component['version']    = '0.001';
+$otw_component['class_name'] = 'OTW_Grid_Manager';
+
+?>
