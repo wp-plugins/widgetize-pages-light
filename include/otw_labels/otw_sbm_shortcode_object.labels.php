8b04fab6e7c2c988a366cb3a78cb0fb68ecc5096
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-22 14:27:03 +0000

    version 1.11
    git-svn-id: https://plugins.svn.wordpress.org/widgetize-pages-light/tags/1.11@863133 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/otw_labels/otw_sbm_shortcode_object.labels.php
@@ -0,0 +1,11 @@
+<?php
+$otw_sbm_shortcode_object->labels['Cancel']=__('Cancel', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Insert']=__('Insert', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Options']=__('Options', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Preview']=__('Preview', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Custom Options']=__('Custom Options', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Invalid shortcode']=__('Invalid shortcode', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Select sidebar']=__('Select sidebar', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Sidebar is required field']=__('Sidebar is required field', 'otw_sbm');
+$otw_sbm_shortcode_object->labels['Close']=__('Close', 'otw_sbm');
+?>
